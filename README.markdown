b0c3df38a4afff6763c01b0cb54d9f65847bc691
Laurent Boileau <developer@laurentboileau.ca>
2019-10-07 19:17:00 -0400

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,17 @@
+# zsh profile
+
+User configuration files for `zsh` on macOS.
+
+## oh-my-zsh
+
+Works best with [oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh). 
+
+Just copy `profile.zsh` to `$ZSH_CUSTOM`
+
+### Plugins
+
+To start using plugins, configure the plugins array in ~/.zshrc:
+
+```
+plugins=(git osx textmate vscode xcode)
+```
---
+++ profile.zsh
@@ -0,0 +1,69 @@
+# Globals
+
+export EDITOR="/usr/local/bin/mate -w"
+
+# Aliases
+
+# Directories
+alias w="cd ~/Workspace"
+
+# oh-my-zsh
+alias zshconfig="mate ~/.zshrc"
+alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# Show active network interfaces
+alias ifactive="ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active'"
+
+# Empty the Trash on all mounted volumes and the main HDD.
+# Also, clear Apple’s System Logs to improve shell startup speed.
+# Finally, clear download history from quarantine. https://mths.be/bum
+alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl; sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+# URL-encode strings
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
+
+# Disable Spotlight
+alias spotoff="sudo mdutil -a -i off"
+# Enable Spotlight
+alias spoton="sudo mdutil -a -i on"
+
+# PlistBuddy alias, because sometimes `defaults` just doesn’t cut it
+alias plistbuddy="/usr/libexec/PlistBuddy"
+
+# Print each PATH entry on a separate line
+alias path='echo -e ${PATH//:/\\n}'
+
+# Functions
+
+# Create a new directory and enter it
+function mkd() {
+    mkdir -p "$@" && cd "$_";
+}
+
+# Create a data URL from a file
+function dataurl() {
+    local mimeType=$(file -b --mime-type "$1");
+    if [[ $mimeType == text/* ]]; then
+        mimeType="${mimeType};charset=utf-8";
+    fi
+    echo "data:${mimeType};base64,$(openssl base64 -in "$1" | tr -d '\n')";
+}
+
+# `o` with no arguments opens the current directory, otherwise opens the given
+# location
+function o() {
+    if [ $# -eq 0 ]; then
+        open .;
+    else
+        open "$@";
+    fi;
+}
+
+# Print an audio file's bit rate
+function bps() {
+    afinfo "$@" | grep "bit rate"
+}
