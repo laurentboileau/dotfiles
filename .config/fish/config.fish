761aa2f8e774f7ed5784766781ce6be4297dea31
Laurent Boileau <developer@laurentboileau.com>
2024-11-23 15:27:53 -0500

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+alias d="developer"
+alias dk="desktop"
+alias dl="downloads"
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    source ~/.asdf/asdf.fish
+end
