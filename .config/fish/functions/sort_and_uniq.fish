9df39a7d2655523a105045f4ec36ed5747163b10
Laurent Boileau <developer@laurentboileau.com>
2024-11-23 15:09:04 -0500

    Add `-r` option

--- .config/fish/functions/sort_and_uniq.fish
+++ .config/fish/functions/sort_and_uniq.fish
@@ -1,12 +1,24 @@
 # Sort and remove duplicates
 
 function sort_and_uniq
-	if test (count $argv) -ne 1
-		echo "Usage: sort_and_uniq <file_path>"
-		return 1
+	set replace_mode 0
+	set file_path ""
+
+	# Parse options
+	for arg in $argv
+		switch $arg
+			case '-r' '--replace'
+				set replace_mode 1
+			case '*'
+				# Assume any non-option argument is the file path
+				set file_path $arg
+		end
 	end
 
-	set file_path $argv[1]
+	if test -z "$file_path"
+		echo "Usage: sort_and_uniq [-r|--replace] <file_path>"
+		return 1
+	end
 
 	if not test -f $file_path
 		echo "File not found: $file_path"
@@ -14,9 +26,15 @@ function sort_and_uniq
 	end
 
 	# Sort the file and remove duplicate lines
-	sort $file_path | uniq > $file_path.sorted
+	# Use `awk` to ensure line endings are preserved
+	set sorted_content (awk '{print $0}' $file_path | sort | uniq)
 
-	mv $file_path.sorted $file_path
-
-	echo "File sorted and duplicates removed: $file_path"
+	if test $replace_mode -eq 1
+		# Replace the contents of the file with the sorted and unique lines
+		printf "%s\n" $sorted_content > $file_path
+		echo "File sorted and duplicates removed: $file_path"
+	else
+		# Output the sorted and unique lines to the terminal
+		printf "%s\n" $sorted_content
+	end
 end
