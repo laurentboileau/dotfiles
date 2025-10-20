3e565c01f867ded78ebad86e50e6a0aa9587fbc3
Laurent Boileau <developer@laurentboileau.com>
2025-10-19 22:34:38 -0400

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
 alias d="developer"
 alias dk="desktop"
 alias dl="downloads"
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 /Users/laurent/bin/mise activate fish | source # added by https://mise.run/fish
