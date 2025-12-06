aeecab5686474aa7c920f9d464a0c76960973eff
Laurent Boileau <developer@laurentboileau.com>
2025-12-06 08:00:03 -0500

    Links

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ln -sv ~/Developer/dotfiles/.config/fish/conf.d/EDITOR.fish ~/.config/fish/conf.d/EDITOR.fish
+ln -sv ~/Developer/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
+ln -sv ~/Developer/dotfiles/.config/fish/functions ~/.config/fish/functions
+
+ln -sv ~/Developer/dotfiles/.config/mise/config.toml ~/.config/mise/config.toml
+
+ln -sv ~/Developer/dotfiles/.config/yt-dlp/config  ~/.config/yt-dlp/config
+
+ln -sv ~/Developer/dotfiles/.gitconfig ~/.gitconfig
+ln -sv ~/Developer/dotfiles/.gitignore-global ~/.gitignore-global
+
+ln -sv ~/Developer/dotfiles/.gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+ln -sv ~/Developer/dotfiles/.gnupg/gpg.conf ~/.gnupg/gpg.conf
