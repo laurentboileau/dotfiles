c67d2b8c45215cf4233c076a7a2b607ed78be885
Laurent Boileau <developer@laurentboileau.com>
2025-12-13 06:40:16 -0500

    Add `mkd` function

--- .config/fish/functions/mkd.fish
+++ .config/fish/functions/mkd.fish
@@ -1,5 +1,26 @@
-# Create a new directory and enter it
+function mkd --description "Create a new directory and changes to it."
+	# Parse arguments
+	argparse h/help -- $argv
+	or return
 
-function mkd
-	mkdir -p "$argv[1]" && cd "$argv[1]";
+	# Handle --help flag
+	if set -q _flag_help
+		echo "Usage: mkd DIRECTORY"
+		echo ""
+		echo "Create a new directory and changes to it."
+		echo ""
+		echo "Arguments:"
+		echo "  DIRECTORY    directory to create and change to"
+		echo ""
+		echo "Options:"
+		echo "  -h/--help  Show this help message"
+		return 0
+	end
+	
+	if test (count $argv) -eq 0
+		mkd --help
+		return 0
+	end
+	
+	mkdir -p "$argv[1]" && cd "$argv[1]"
 end
