c93b64f57882a735c44742113240def9bc605212
Laurent Boileau <developer@laurentboileau.com>
2025-12-13 08:01:08 -0500

    Add `delete_where_from` function

--- .config/fish/functions/delete_where_from.fish
+++ .config/fish/functions/delete_where_from.fish
@@ -1,6 +1,29 @@
-# Delete the `com.apple.metadata:kMDItemWhereFroms` extended attribute on a
-# given file
+function delete_where_from --description "Delete the com.apple.metadata:kMDItemWhereFroms extended attribute on files."
+    # Parse arguments
+    argparse h/help -- $argv
+    or return
 
-function delete_where_from
-	xattr -d com.apple.metadata:kMDItemWhereFroms $argv[1]
+    # Handle --help flag
+    if set -q _flag_help
+        echo "Usage: delete_where_from FILE [FILE...]"
+        echo ""
+        echo "Delete the com.apple.metadata:kMDItemWhereFroms extended attribute on files."
+        echo ""
+        echo "Arguments:"
+        echo "  FILE    One or more files"
+        echo ""
+        echo "Options:"
+        echo "  -h/--help  Show this help message"
+        return 0
+    end
+    
+    if test (count $argv) -eq 0
+        delete_where_from --help
+        return 0
+    end
+
+    # Process each file
+    for filepath in $argv
+        xattr -d com.apple.metadata:kMDItemWhereFroms $filepath
+    end
 end
