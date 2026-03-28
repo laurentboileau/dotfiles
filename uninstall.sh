16a2a883b450a710b0418cc0de85b67dbba3b4ab
Laurent Boileau <developer@laurentboileau.com>
2026-03-28 07:14:27 -0400

    Add uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+rm -v ~/.config/fish/conf.d/brew.fish
+rm -v ~/.config/fish/conf.d/EDITOR.fish
+rm -v ~/.config/fish/conf.d/mise.fish
+rm -v ~/.config/fish/conf.d/myabbrs.fish
+rm -v ~/.config/fish/config.fish
+rm -v ~/.config/fish/functions
+
+rm -v ~/.config/mise/config.toml
+
+rm -v ~/.config/yt-dlp/config
+
+rm -v ~/.gitconfig
+rm -v ~/.gitignore-global
+
+rm -v ~/.gnupg/gpg-agent.conf
+rm -v ~/.gnupg/gpg.conf
